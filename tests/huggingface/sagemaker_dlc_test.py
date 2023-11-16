dae8dce9f47a5d440b820f15e558b5052d133d32
Cho <choeric@amazon.com>
2023-11-16 13:01:38 -0500

    TGI Sanity Tests

--- tests/huggingface/sagemaker_dlc_test.py
+++ tests/huggingface/sagemaker_dlc_test.py
@@ -4,8 +4,13 @@ import argparse
 import time
 import signal
 import json
+import os
+import pytest
 
 from sagemaker.huggingface import HuggingFaceModel
+
+
+logging.basicConfig(stream=sys.stdout, format="%(message)s", level=logging.INFO)
 
 
 class TimeoutError(Exception):
@@ -15,14 +20,11 @@ class TimeoutError(Exception):
 def timeout_handler(signum, frame):
     raise TimeoutError("Test timed out")
 
-
-# Running test
 def run_test(args):
     endpoint_name = args.model_id.replace("/","-") + "-" + time.strftime("%Y-%m-%d-%H-%M-%S", time.gmtime())
 
     hub = {
         'HF_MODEL_ID':args.model_id,
-        'HF_TASK':args.task,
         'SM_NUM_GPUS':args.num_gpus
     }
 
@@ -53,17 +55,34 @@ def run_test(args):
         predictor.delete_endpoint()
         signal.alarm(0)
 
+@pytest.mark.parametrize("model_id", ["bigscience/bloom-560m", "EleutherAI/gpt-neox-20b", "google/flan-t5-xxl"])
+def test_blank(
+        model_id: str,
+        instance_type: str = "ml.g5.12xlarge",
+        num_gpus: str = "4",
+        timeout: str = "1500"):
+    image_uri = os.getenv("IMAGE_URI")
+    test_role_arn = os.getenv("TEST_ROLE_ARN")
+    assert image_uri, f"Please set IMAGE_URI environment variable."
+    assert test_role_arn, f"Please set TEST_ROLE_ARN environment variable."
+    args = argparse.Namespace(
+        image_uri=image_uri,
+        instance_type=instance_type,
+        model_id=model_id,
+        num_gpus=num_gpus,
+        role=test_role_arn,
+        timeout=timeout)
+
+    logging.info(f"Running sanity test with the following args: {args}.")
+    run_test(args)
+
 
 if __name__ == '__main__':
-    logging.basicConfig(stream=sys.stdout, format="%(message)s", level=logging.INFO)
-
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument("--image_uri", type=str, required=True)
-    arg_parser.add_argument("--region", type=str,required=True)
     arg_parser.add_argument("--instance_type", type=str, required=True)
     arg_parser.add_argument("--model_id", type=str, required=True)
     arg_parser.add_argument("--num_gpus", type=str, required=True)
-    arg_parser.add_argument("--task", type=str, required=True)
     arg_parser.add_argument("--role", type=str, required=True)
     arg_parser.add_argument("--timeout", type=str, required=True)
 
